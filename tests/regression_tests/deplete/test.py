450f0e80acbadeb46f2e652f12df4a7d0f9f3501
Andrew Johnson <drewej@protonmail.com>
2020-06-18 16:05:16 -0400

    Parametrize depletion regression test w/ and w/o multiprocessing

--- tests/regression_tests/deplete/test.py
+++ tests/regression_tests/deplete/test.py
@@ -5,6 +5,7 @@ import shutil
 from pathlib import Path
 
 import numpy as np
+import pytest
 import openmc
 from openmc.data import JOULE_PER_EV
 import openmc.deplete
@@ -13,7 +14,17 @@ from tests.regression_tests import config
 from example_geometry import generate_problem
 
 
-def test_full(run_in_tmpdir):
+@pytest.fixture(scope="module")
+def problem():
+    n_rings = 2
+    n_wedges = 4
+
+    # Load geometry from example
+    return generate_problem(n_rings, n_wedges)
+
+
+@pytest.mark.parametrize("multiproc", [True, False])
+def test_full(run_in_tmpdir, problem, multiproc):
     """Full system test suite.
 
     Runs an entire OpenMC simulation with depletion coupling and verifies
@@ -25,11 +36,7 @@ def test_full(run_in_tmpdir):
 
     """
 
-    n_rings = 2
-    n_wedges = 4
-
-    # Load geometry from example
-    geometry, lower_left, upper_right = generate_problem(n_rings, n_wedges)
+    geometry, lower_left, upper_right = problem
 
     # OpenMC-specific settings
     settings = openmc.Settings()
@@ -54,7 +61,9 @@ def test_full(run_in_tmpdir):
     power = 2.337e15*4*JOULE_PER_EV*1e6  # MeV/second cm from CASMO
 
     # Perform simulation using the predictor algorithm
-    openmc.deplete.PredictorIntegrator(op, dt, power).integrate()
+    openmc.deplete.PredictorIntegrator(
+        op, dt, power, with_multiprocessing=multiproc
+    ).integrate()
 
     # Get path to test and reference results
     path_test = op.output_dir / 'depletion_results.h5'
